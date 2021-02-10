a9ce12564b669878860e6e34f067a86b70e606fa
Blake Chellew <blakechellew@dhcp-10-13-147-109.cv.reshsg.uci.edu>
2021-02-09 21:06:17 -0800

    Fixed some issues and expanded integration ranges

--- python_scripts/radiative_transfer.py
+++ python_scripts/radiative_transfer.py
@@ -65,8 +65,8 @@ b = 40 * np.pi / 180  # 40 degrees -> radians
 
 
 # phase function
-def henyey(cos_theta, g):
-    return (1 - g**2) / (1 + g**2 - 2*g*cos_theta)**(3/2) / (4*np.pi)
+def henyey(cos_xi, g):
+    return (1 - g**2) / (1 + g**2 - 2*g*cos_xi)**(3/2) / (4*np.pi)
 
 
 sig_dust = 250  # parsecs (from density eqn)
@@ -74,25 +74,23 @@ sig_dust = 250  # parsecs (from density eqn)
 V_band_wav = 5510  # A
 density_prefactor = 0.3 / dust_cross_f(V_band_wav) / sig_dust / np.sqrt(2 * np.pi)  # cm^-2 * pc^-1
 
-# CHECKED THROUGH HERE (units and equations)
-
 
 # eqn A1
+# z should be in pc
 # density: rho ~ exp(-z^2 / 2 / sig_dust^2).
 # I took sigma out of the integral and integrated analytically.
 # units of cross section should cancel out b/c it's also in the density prefactor.
 def tau_f(lamb, z):
-    cross_section = dust_cross_f(lamb)
+    cross_section = dust_cross_f(lamb)  # cm^2
     tau_lambda = density_prefactor * cross_section * sig_dust * np.sqrt(2 * np.pi) / 2 \
         * (1 - special.erf(z / sig_dust / np.sqrt(2)))
-    return tau_lambda
-
+    return tau_lambda  # unitless
 
 # reverse: find z given tau
 # (see paper calculations)
 # (maybe not needed any more)
 def z_of_tau(lamb, tau):
-    cross_section = dust_cross_f(lamb)
+    cross_section = dust_cross_f(lamb)  # cm^2
     return sig_dust * np.sqrt(2) * special.erfinv(1 - 2*tau/(density_prefactor * cross_section * sig_dust * np.sqrt(2*np.pi)))
 
 
@@ -113,19 +111,12 @@ def cos_xi(z, R, theta, z_s):
     denom = np.sqrt(term1 * term2)
     result = numer / denom
 
+    # take the limit when there are issues
     if type(z) == np.ndarray or type(z_s) == np.ndarray:
         np.putmask(result, z == z_s, -np.cos(theta))
-    #    idx = np.nonzero(z == z_s)
-    #    print("indices")
-    #    print(idx)
-    #    print(result.shape)
-    #    if type(theta) == np.ndarray:
-    #        result[idx] = -np.cos(theta[idx])
-    #    else:
-    #        result[idx] = -np.cos(theta)
-    #    # avoid singularity; see notes
     elif z == z_s:
         return -np.cos(theta)
+
     return result
 
 
@@ -134,11 +125,10 @@ a_300 = 0.9
 a_1350 = 0.1
 
 
-# surface power density (any normalization factor will cancel out later)
+# surface power density (arbitrary units; any normalization factor will cancel out later)
 # it's a function of height z_s
 def surface_power_fn(bc03, z_s, lamb):  # z_s in pc
     return bc03(lamb) * (a_300 * np.exp(-z_s / 300) + a_1350 * np.exp(-z_s / 1350))
-
 
 # I found the derivative analytically
 def surface_power_deriv(bc03, z_s, lamb):
@@ -146,19 +136,30 @@ def surface_power_deriv(bc03, z_s, lamb):
 
 
 # eqn A4: integrand
+# units: angstroms * units of sigma after integration
 def i_tir_integrand(lamb, z_s, bc03):
     prefactor = 1 / (8*np.pi) / np.sin(np.abs(b))
     term1 = 1 - dust_albedo_f(lamb)
     term2 = surface_power_deriv(bc03, z_s, lamb)
     tau_zs = tau_f(lamb, z_s)
     tau_0 = tau_f(lamb, 0)
-    term3 = 2 + tau_zs*special.expi(tau_zs) - np.exp(tau_zs) \
-            + (tau_0 - tau_zs)*special.expi(tau_0 - tau_zs) - np.exp(tau_0 - tau_zs)
+    term3 = 2 + tau_zs * special.expi(tau_zs) - np.exp(tau_zs) \
+        + (tau_0 - tau_zs) * special.expi(tau_0 - tau_zs) - np.exp(tau_0 - tau_zs)
+    term3_2 = (tau_0 - tau_zs)*special.expi(tau_zs - tau_0) + np.exp(tau_zs - tau_0) \
+        + tau_zs * special.expi(tau_zs) - np.exp(tau_zs)
+
+    if type(z_s) == np.ndarray or type(lamb) == np.ndarray:
+        np.putmask(term3, tau_zs > tau_0, term3_2)
+    elif tau_zs > tau_0:
+        term3 = term3_2
+
     result = prefactor * term1 * term2 * term3
     return result
 
+
 # eqn A7, part 1: just the integrand.
-# note that we need to integrate over z_s also.
+# note that we integrate over z_s also.
+# units: parsecs * units of sigma
 def i_sca_integrand(theta, tau, R, z_s, lamb, bc03):
     prefactor = (1 / np.sin(np.abs(b))) * dust_albedo_f(lamb)
     z = z_of_tau(lamb, tau)
@@ -180,7 +181,6 @@ def i_sca_integrand(theta, tau, R, z_s, lamb, bc03):
 
 # eqn A7, part 2: do the integral for given wavelength
 def i_sca(lamb, bc03):
-
 
     """
     # plot integrand for I_sca:
@@ -206,7 +206,7 @@ def i_sca(lamb, bc03):
     plt.show()
 
     # plot vs. z_s (for given tau, theta, R)
-    zs_vals = np.linspace(-3, 1000, 50)
+    zs_vals = np.linspace(-100, 1000, 50)
     tau_temp = 0.1
     theta_temp = np.pi
     R_temp = 1
@@ -230,13 +230,13 @@ def i_sca(lamb, bc03):
     x_min = 0
     x_max = 2*np.pi
     x = np.linspace(x_min, x_max, num_div)  # theta grid, 0 to 2pi
-    y_min = .18  # get better estimate of lower bound
+    y_min = .01  # get better estimate of lower bound
     y_max = tau_f(lamb, 0)-.00001
     y = np.linspace(y_min, y_max, num_div)  # tau grid, 0 to tau(0)
     z_min = .01
     z_max = 1000
-    z = np.linspace(.01, 1000, num_div)  # R grid
-    v_min = -3
+    z = np.linspace(z_min, z_max, num_div)  # R grid
+    v_min = -100
     v_max = 1000
     v = np.linspace(v_min, v_max, num_div)  # z_s grid, -inf to inf  (peak depends on tau)
 
@@ -254,13 +254,14 @@ def i_sca(lamb, bc03):
     # print("number of nans")
     # print(np.count_nonzero(np.isnan(ww)))
 
-    inner = [integrate.simps(ww_x, x) for ww_x in ww]
-    middle1 = [integrate.simps(ww_y, y) for ww_y in inner]
-    middle2 = [integrate.simps(ww_z, z) for ww_z in middle1]
-    result = integrate.simps(middle2, v)
+    # inner = [integrate.simps(ww_x, x) for ww_x in ww]
+    # middle1 = [integrate.simps(ww_y, y) for ww_y in inner]
+    # middle2 = [integrate.simps(ww_z, z) for ww_z in middle1]
+    # result = integrate.simps(middle2, v)
+    # return result
 
-    return result
 
+# CHECKED THROUGH HERE (units and equations)
 
 # loop through the bc03 spectra
 for p in paths[:1]:
@@ -270,15 +271,27 @@ for p in paths[:1]:
     bc03 = a[:, 1]
     bc03_f = interp1d(wav, bc03, kind='cubic')
 
+    # plt.plot(wav, bc03_f(wav))
+    # plt.show()
+
+    # testing A4:
+    #i_tir = i_tir_integrand(6000, 2, bc03_f)
+    #print("I tir test")
+    #print(i_tir)
+    #exit(0)
+
+
+
     # compute total infrared radiation
     # (0 to inf throws an error)
     # (range of lambda for dust model is 10^-4 to 10^4 microns, or 1 to 10^8 angstroms)
     # (note: range of lambda for bc03 is 91 to 1.6e6)
+
     """
     # plot integrand for I_TIR:
     # vars: z, lambda, z_s
     # plot vs. lambda (for given z, zs)
-    lambda_vals = np.linspace(100, 6*10**4, 50)
+    lambda_vals = np.linspace(91, 6*10**4, 50)
     z_s_temp = 1.5
     plt.plot(lambda_vals, i_tir_integrand(lambda_vals, z_s_temp, bc03_f))
     plt.title('I_TIR integrand vs. wavelength')
@@ -293,12 +306,12 @@ for p in paths[:1]:
     plt.show()
     """
 
-    num_div = 50
+    num_div = 100
     y_min = 100
-    y_max = 6*10**4  # lambda grid, 91 to 10**4 A
+    y_max = 10**6  # 6*10**4  # lambda grid, 91 to 10**4 A
     y = np.linspace(y_min, y_max, num_div)  # min, max, n
-    z_min = -3
-    z_max = 70
+    z_min = -1000
+    z_max = 1000
     z = np.linspace(z_min, z_max, num_div)  # z_s grid, -inf to inf
     yy, zz = np.meshgrid(y, z)
     ww = i_tir_integrand(yy, zz, bc03_f)
@@ -307,7 +320,7 @@ for p in paths[:1]:
     # integrate on simple grid
     y_div = (y_max - y_min) / (num_div - 1)
     z_div = (z_max - z_min) / (num_div - 1)
-    I_tir = np.sum(ww) * y_div * z_div
+    I_tir = np.sum(ww) * y_div * z_div  # units of angstroms * sigma
 
     # inner = [integrate.simps(ww_y, y) for ww_y in ww]
     # I_tir = integrate.simps(inner, z)
@@ -316,14 +329,12 @@ for p in paths[:1]:
     print(I_tir)
 
     # convert to 100 micron (there is an associated uncertainty)
-    nu_I_nu_100 = .52 * I_tir
-
-    # REVIEWED THROUGH HERE
+    nu_I_nu_100 = .52 * I_tir  # units?
 
     print("starting integral")
 
     # this will take a long time
-    wavelength_partial = wavelength[1000:1001]
+    wavelength_partial = wavelength[1005:1006]
 
     i_sca_array = np.array([i_sca(lamb, bc03_f) for lamb in wavelength_partial])
 
@@ -340,4 +351,5 @@ for p in paths[:1]:
     # save the result
     save_path = '/Users/blakechellew/Documents/DustProject/BrandtFiles/radiative/'
     save_path += p.split('/')[-1].rsplit('.spec')[0] + '.npy'
-    # np.save(save_path, result)
+    print(save_path)
+    np.save(save_path, alphas)
