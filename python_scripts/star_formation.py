cf232ee600d68af3a4f030b408235f29b7a102a6
Blake Chellew <blakechellew@Blakes-MBP-14.home>
2021-06-30 14:17:50 -0500

    Added bootstrapping to compute error bars

--- python_scripts/star_formation.py
+++ python_scripts/star_formation.py
@@ -6,191 +6,215 @@ import numpy as np
 import matplotlib.pyplot as plt
 import glob
 from scipy.interpolate import interp1d
-from scipy.optimize import minimize
+from scipy import stats # TEST
+
+# parameters
+# (poly_degree is for the polynomial added to the linear combo)
+# (continuum_degree is for ???)
+poly_degree = -1  # 1 is linear, etc.
+poly_order = poly_degree + 1
+continuum_deg = 5
+continuum_order = continuum_deg + 1
+bootstrap = True
+num_bootstrap_samples = 100
+num_spectra = 3
+min_wav = 6000
+max_wav = 9000  # 10000
 
 # paths for bc03 spectra
-paths = glob.glob('/Users/blakechellew/Documents/DustProject/BrandtFiles/bc03/*.spec')
+paths_bc03 = glob.glob('/Users/blakechellew/Documents/DustProject/BrandtFiles/bc03/*.spec')
 
-# print names of spectra:
-# for path in paths:
-#    print(path)
-
-# plot the model spectra:
-'''
-for p in paths[:10]:
-    if 'ssp_5Myr_z008' in p:
-        a = np.loadtxt(p)
-
-        wav = a[:, 0]
-        values = a[:, 1]
-
-        plt.plot(wav, values, drawstyle='steps')
-        plt.xlim(3500, 10000)
-        plt.show()
-'''
-
+if bootstrap:
+    alphas_bootstrap = np.load('../alphas_and_stds/bootstrap_alphas_boss_iris_2d_012720.npy')
+    alpha_stds_bootstrap = np.load('../alphas_and_stds/bootstrap_alpha_stds_boss_iris_2d_012720.npy')
 # alphas and stds (boss):
-alphas = [np.load('../alphas_and_stds/alphas_boss_102019.npy')]
-alpha_stds = [np.load('../alphas_and_stds/alpha_stds_boss_2d_102019.npy')]
+alphas_boss = np.load('../alphas_and_stds/alphas_boss_iris_2d_012720_10.npy')
+alpha_stds_boss = np.load('../alphas_and_stds/alpha_stds_boss_iris_2d_012720_10.npy')
 wavelength = np.load('../alphas_and_stds/wavelength_boss.npy')
 
-# limit the wavelength range to 4000 to 9000 A:
-for i in range(len(alphas)):
-    alphas[i] = alphas[i][(wavelength > 4000) & (wavelength < 9000)]
-    alpha_stds[i] = alpha_stds[i][(wavelength > 4000) & (wavelength < 9000)]
-wavelength = wavelength[(wavelength > 4000) & (wavelength < 9000)]
+# alphas_boss[:] = 1
+# alpha_stds_boss[:] = 1
 
-# mask emission lines
-emission_line_mask = np.zeros(len(wavelength), dtype=int)
-emission_lines = [3727, 4863, 4960, 5008, 5877, 6550, 6565, 6585, 6718, 6733]
-for line in emission_lines:
-    peak_idx = np.argmin(np.abs(wavelength-line))
-    emission_line_mask[peak_idx-3:peak_idx+4] = 1
-for i in range(len(alphas)):
-    alphas[i] = alphas[i][np.logical_not(emission_line_mask)]
-    alpha_stds[i] = alpha_stds[i][np.logical_not(emission_line_mask)]
-wavelength = wavelength[np.logical_not(emission_line_mask)]
+"""
+# TEST: plot to see if bootstrap alphas match
+for i in range(num_bootstrap_samples):
+    plt.plot(wavelength, alphas_bootstrap[i], 'g')
+plt.plot(wavelength, alphas_boss, 'k')
+plt.ylim(0, .6)
+plt.show()
+"""
 
-# for adding a polynomial
-poly_degree = 3 # 1 is linear, etc.
-poly_order = poly_degree + 1
-poly_order = 0 # TEMP
+# TEST
+chai_less_1 = []
 
-num_model_spectra = 3
-normalize_index = 18 # divide by this spectrum later
+# TEST: 1d alphas
+#alphas = np.load('../alphas_and_stds/alphas_boss_iris_1d_91119_10.npy')
+#alpha_stds = np.load('../alphas_and_stds/alpha_stds_boss_iris_1d_91119_10.npy')
 
-# if only 3:
-# cst_6gyr_z02 [idx 29]
-# t9e9_12gyr_z02 [idx 21]
-# t5e9_12gyr_z02 [idx 22]
+def alphas_to_coeffs(alphas, alpha_stds, wavelength, paths, showPlots=True):
 
-if num_model_spectra == 3:
+    # limit the wavelength range to 4000 to 10000 A:
+    # (BOSS goes from 3550 to 10,400)
+    alphas = alphas[(wavelength > min_wav) & (wavelength < max_wav)]
+    alpha_stds = alpha_stds[(wavelength > min_wav) & (wavelength < max_wav)]
+    wavelength = wavelength[(wavelength > min_wav) & (wavelength < max_wav)]
+
+    # mask emission lines
+    emission_line_mask = np.zeros(len(wavelength), dtype=int)
+    emission_lines = [3727, 4863, 4960, 5008, 5877, 6550, 6565, 6585, 6718, 6733]
+    for line in emission_lines:
+        peak_idx = np.argmin(np.abs(wavelength-line))
+        emission_line_mask[peak_idx-3:peak_idx+4] = 1
+    alphas = alphas[np.logical_not(emission_line_mask)]
+    alpha_stds = alpha_stds[np.logical_not(emission_line_mask)]
+    wavelength = wavelength[np.logical_not(emission_line_mask)]
+
+    # if only 3 model spectra:
+    # cst_6gyr_z02 [idx 29]
+    # t9e9_12gyr_z02 [idx 21]
+    # t5e9_12gyr_z02 [idx 22]
     paths = [paths[29],
              paths[21],
              paths[22]]
-    normalize_index = 2
 
-# load the spectra and interpolate to same wavelength range
-# emission lines are effectively masked bc those wavelengths are removed
-model_spectra = np.zeros((len(paths)+poly_order, len(wavelength)))
-for i, p in enumerate(paths):
-    a = np.loadtxt(p)
-    wav = a[:, 0]
-    values = a[:, 1]
-    f = interp1d(wav, values, kind='cubic')
-    model_spectra[i] = np.array([f(w) for w in wavelength])
+    # load the spectra and interpolate to same wavelength range
+    # emission lines are effectively masked bc those wavelengths are removed
+    model_spectra = np.zeros((len(paths)+poly_order, len(wavelength)))
+    for i, p in enumerate(paths):
+        a = np.loadtxt(p)
+        wav = a[:, 0]
+        values = a[:, 1]
+        f = interp1d(wav, values, kind='cubic')
+        model_spectra[i] = np.array([f(w) for w in wavelength])
 
-# subtract continuum from all the spectra
-continuum_deg = 4
-continuum_order = continuum_deg + 1
-for i in range(len(paths)):
-    coeffs_i = np.polyfit(wavelength, model_spectra[i], continuum_deg)
-    continuum_fit = 0
-    for j in range(continuum_order):
-        continuum_fit += coeffs_i[-1-j] * np.power(wavelength, j)
-    model_spectra[i] /= continuum_fit
-# same for alphas (need to account for errors better)
-for i in range(len(alphas)):
-    coeffs_i = np.polyfit(wavelength, alphas[i], continuum_deg)
+    # subtract continuum from the model spectra
+    for i in range(len(paths)):
+        coeffs_i = np.polyfit(wavelength, model_spectra[i], continuum_deg)
+        continuum_fit = 0
+        for j in range(continuum_order):
+            continuum_fit += coeffs_i[-1-j] * np.power(wavelength, j)
+        assert continuum_fit.shape == wavelength.shape
+        model_spectra[i] /= continuum_fit
+
+    # subtract continuum from the alphas
+    coeffs_i = np.polyfit(wavelength, alphas, continuum_deg, w=1/alpha_stds)
     continuum_fit = 0
     for j in range(continuum_order):
         continuum_fit += coeffs_i[-1 - j] * np.power(wavelength, j)
-    alphas[i] /= continuum_fit
-    alpha_stds[i] /= continuum_fit
+    alphas /= continuum_fit
+    alpha_stds /= continuum_fit
 
-# find best-fit linear combination [plus polynomial] using max likelihood estimator
-# and plot the best-fit spectrum against actual spectrum
+    # find best-fit linear combination using max likelihood estimator
+    # and plot the best-fit spectrum against actual spectrum
+    num_spectra = len(paths)+poly_order
+    A = np.zeros((num_spectra, num_spectra))
+    for i in range(num_spectra):
+        for j in range(num_spectra):
+            A[i, j] = np.nansum(model_spectra[i]*model_spectra[j]/np.power(alpha_stds, 2))
+    b = np.zeros(num_spectra)
+    for i in range(num_spectra):
+        b[i] = np.nansum(alphas*model_spectra[i]/np.power(alpha_stds, 2))
 
-# scale them based on total flux in the BOSS wavelength range (maybe change to 4000 to 9000)
-# (the model spectra were interpolated onto that range above).
-# that way, the coefficient ratio represents the flux ratio (between/among the source models).
-if poly_order != 0:  #this is unnecessary if we are doing continuum normalization
-    for i in range(len(paths)):
-        model_spectra[i] = model_spectra[i] / np.sum(model_spectra[i]) * np.sum(model_spectra[normalize_index])
-    for i in range(poly_order):
-        model_spectra[-i-1] = np.power(wavelength, i)
+    print("rank", np.linalg.lstsq(A, b)[2])
+    coeffs = np.linalg.solve(A, b)
+    # coeffs = np.linalg.lstsq(A, b)[0]
+    print("coefficients")
+    print(coeffs)
 
-num_spectra = len(paths)+poly_order
-A = np.zeros((num_spectra, num_spectra))
-for i in range(num_spectra):
-    for j in range(num_spectra):
-        A[i, j] = np.nansum(model_spectra[i]*model_spectra[j]/np.power(alpha_stds[0], 2))
-b = np.zeros(num_spectra)
-for i in range(num_spectra):
-    b[i] = np.nansum(alphas[0]*model_spectra[i]/np.power(alpha_stds[0], 2))
+    # A happens to be the inverse covariance matrix.
+    # (see paper notes)
+    Cov = np.linalg.inv(A)
+    print("covariance")
+    print(Cov)
 
-#coeffs = np.linalg.lstsq(A, b)[0]
-print("rank", np.linalg.lstsq(A, b)[2])
-coeffs = np.linalg.solve(A, b)
-print("coefficients")
-print(coeffs)
+    # variance of the function combining the 3 best-fit coeffs
+    # calculate separately at each wavelength
+    # here the "coeffs" are value of model spectrum at given wavelength
+    fitted_model_vars = np.zeros(wavelength.shape)
+    for i in range(len(wavelength)):
+        a = model_spectra[:, i]
+        a_col = a.reshape(len(a), 1)
+        a_row = a.reshape(1, len(a))
+        var_i = a_row.dot(Cov).dot(a_col)
+        fitted_model_vars[i] = var_i
+    fitted_model_stds = np.sqrt(fitted_model_vars)
 
-print(coeffs.shape)
+    # plot the best fit spectrum:
+    weighted_model_spectra = np.copy(model_spectra)
 
-# I think A happens to be the inverse covariance matrix.
-# (see paper notes)
-Cov = np.linalg.inv(A)
-print("covariance")
-print(Cov)
+    for i in range(len(coeffs)):
+        weighted_model_spectra[i] *= coeffs[i]
+    best_fit_model = np.sum(weighted_model_spectra, axis=0)
 
-# variance of the "function combining the 3 best-fit coeffs
-# calculate separately at each wavelength
-# here the "coeffs" are value of model spectrum at given wavelength
-fitted_model_vars = np.zeros(wavelength.shape)
-for i in range(len(wavelength)):
-    a = model_spectra[:, i]
-    a_col = a.reshape(len(a), 1)
-    a_row = a.reshape(1, len(a))
-    var_i = a_row.dot(Cov).dot(a_col)
-    fitted_model_vars[i] = var_i
-fitted_model_stds = np.sqrt(fitted_model_vars)
+    # find the chai squared:
+    print("chai squared")
+    chai_squared = np.nansum(np.divide(np.power(best_fit_model - alphas, 2), np.power(alpha_stds, 2)))
+    print(chai_squared)
+    print("per degree of freedom (for 3 spectra)")
+    chai_df = chai_squared / (len(wavelength) + poly_order)
+    print(chai_df)
 
+    # TEST
+    if chai_df < 1:
+        chai_less_1.append(chai_df)
 
+    # find correlation:
+    corr = stats.pearsonr(alphas, best_fit_model)
+    print("correlation coeff:", corr)
 
+    if showPlots:  # if bootstrapping there will be too many plots
+        plt.plot(wavelength, alphas, 'k', drawstyle='steps')
+        plt.plot(wavelength, best_fit_model, 'r', drawstyle='steps')
+        plt.fill_between(wavelength, best_fit_model + 3*fitted_model_stds, best_fit_model - 3*fitted_model_stds, color='r', alpha=0.5, step='pre')
+        plt.plot(wavelength, alpha_stds, 'g', drawstyle='steps')
+        #plt.plot(wavelength, best_fit_model + 3*fitted_model_stds, 'r', drawstyle='steps')
+        #plt.plot(wavelength, best_fit_model - 3*fitted_model_stds, 'r', drawstyle='steps')
+        plt.xlim(3500, 10000)
+        plt.ylim(0, 2)
+        plt.show()
 
-# constrain coefficients to be positive using curve_fit
+    return coeffs
+
+if bootstrap:
+    coeffs_array = np.zeros((num_bootstrap_samples, num_spectra))
+    for i in range(num_bootstrap_samples):
+        print("sample number", i)
+        coeffs_array[i] = alphas_to_coeffs(alphas_bootstrap[i], alpha_stds_boss, wavelength, paths_bc03, showPlots=False)  # TEST: non-bootstrap stds
+    print(coeffs_array)
+
+    # combine the coefficients:
+    avg_coeffs = np.mean(coeffs_array, axis=0)
+    coeff_stds = np.std(coeffs_array, axis=0)
+    print("coeffs with stds:")
+    print(avg_coeffs)
+    print(coeff_stds)
+
+    # TEST
+    print(chai_less_1)
+
+else:
+    coeffs = alphas_to_coeffs(alphas_boss, alpha_stds_boss, wavelength, paths_bc03)
+    print("returned coeffs:")
+    print(coeffs)
+
+################
+
 """
+# try fitting with curve_fit instead:
 def func(x, a, b, c):
     spec_1 = interp1d(wavelength, model_spectra[0], kind='cubic')
     spec_2 = interp1d(wavelength, model_spectra[1], kind='cubic')
     spec_3 = interp1d(wavelength, model_spectra[2], kind='cubic')
     return a*spec_1(x) + b*spec_2(x) + c*spec_3(x)
 from scipy.optimize import curve_fit
-coeffs, pcov = curve_fit(func, wavelength, alphas[0], bounds=(0, 20))
+coeffs, pcov = curve_fit(func, wavelength, alphas, bounds=(-30, 30), sigma=alpha_stds)
 print("new coeffs")
 print(coeffs)
 """
 
-# plot the best fit spectrum:
-# best_fit_model =
-weighted_model_spectra = np.copy(model_spectra)
-
-for i in range(len(coeffs)):
-    weighted_model_spectra[i] *= coeffs[i]
-best_fit_model = np.sum(weighted_model_spectra, axis=0)
-
-# find the chai squared:
-print("chai squared")
-chai_squared = np.nansum(np.divide(np.power(best_fit_model - alphas[0], 2), np.power(alpha_stds[0], 2)))
-print(chai_squared)
-print("per degree of freedom (for 3 spectra)")
-chai_df = chai_squared / (len(wavelength) + poly_order)
-print(chai_df)
-    
-plt.plot(wavelength, best_fit_model, 'r', drawstyle='steps')
-plt.fill_between(wavelength, best_fit_model + 3*fitted_model_stds, best_fit_model - 3*fitted_model_stds, color='r', alpha=0.5, step='pre')
-#plt.plot(wavelength, best_fit_model + 3*fitted_model_stds, 'r', drawstyle='steps')
-#plt.plot(wavelength, best_fit_model - 3*fitted_model_stds, 'r', drawstyle='steps')
-plt.plot(wavelength, alphas[0], 'k', drawstyle='steps')
-plt.xlim(3500, 10000)
-plt.ylim(0, 1)
-plt.show()
-
-
-exit(0)
-
 # LICK INDEX STUFF BELOW THIS POINT #############################################
 
+"""
 # some important lick index tuples:
 h_beta_bounds = (4847.875, 4876.625, 4827.875, 4847.875, 4876.625, 4891.625, 0)
 mg1_bounds = (5069.125, 5134.125, 4895.125, 4957.625, 5301.125, 5366.125, 1)
@@ -341,3 +365,4 @@ for p1 in paths:
             print(min_res.x)
             print(p1)
             print(p2)
+"""
