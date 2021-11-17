4e27db729515eeda206b5bcbb2b38cb6ba70e981
Blake Chellew <blakechellew@dhcp-v243-032.pv.reshsg.uci.edu>
2021-11-16 22:56:21 -0800

    Add new parameter to script calls (whether to find correction factor)

--- python_scripts/script_looper.py
+++ python_scripts/script_looper.py
@@ -16,34 +16,42 @@ thresholds = ['10', '15', '20', '25', '30']
 threshold_labels = ['10', '15', '20', '25', '30'] #in case there is a decimal point
 
 #generate the alphas:
+"""
 os.system('python generate_alphas_ahab.py 1d 0 sdss_1d_' + savekey + ' 10 0 0')
 os.system('python generate_alphas_ahab.py iris_1d 0 sdss_iris_1d_' + savekey + ' 10 0 0')
 os.system('python generate_alphas_ahab.py 2d 0 sdss_2d_' + savekey + ' 10 0 0')
 os.system('python generate_alphas_ahab.py iris_2d 0 sdss_iris_2d_' + savekey + ' 10 0 0')
-for i in range(len(thresholds)):
-    os.system('python generate_alphas_ahab.py iris_1d 1 boss_iris_1d_' + savekey + '_' + threshold_labels[i] + ' ' + thresholds[i] + ' 0 0')
-    os.system('python generate_alphas_ahab.py iris_2d 1 boss_iris_2d_' + savekey + '_' + threshold_labels[i] + ' ' + thresholds[i] + ' 0 0')
-os.system('python generate_alphas_ahab.py iris_2d 1 boss_iris_2d_north_' + savekey + ' 10 1 0')
-os.system('python generate_alphas_ahab.py iris_2d 1 boss_iris_2d_south_' + savekey + ' 10 2 0')
+"""
+# for i in range(len(thresholds)):
+    # os.system('python generate_alphas_ahab.py iris_1d 1 boss_iris_1d_' + savekey + '_' + threshold_labels[i] + ' ' + thresholds[i] + ' 0 0 0')
+    # os.system('python generate_alphas_ahab.py iris_2d 1 boss_iris_2d_' + savekey + '_' + threshold_labels[i] + ' ' + thresholds[i] + ' 0 0 0')
+# os.system('python generate_alphas_ahab.py iris_2d 1 boss_iris_2d_north_' + savekey + ' 10 1 0')
+# os.system('python generate_alphas_ahab.py iris_2d 1 boss_iris_2d_south_' + savekey + ' 10 2 0')
 
 #intermediate bootstrap step for all of these:
+"""
 os.system('python generate_alphas_ahab.py 1d 0 sdss_1d_' + savekey + ' 10 0 1')
 os.system('python generate_alphas_ahab.py iris_1d 0 sdss_iris_1d_' + savekey + ' 10 0 1')
 os.system('python generate_alphas_ahab.py 2d 0 sdss_2d_' + savekey + ' 10 0 1')
 os.system('python generate_alphas_ahab.py iris_2d 0 sdss_iris_2d_' + savekey + ' 10 0 1')
+"""
 for i in range(len(thresholds)):
-    os.system('python generate_alphas_ahab.py iris_1d 1 boss_iris_1d_' + savekey + '_' + threshold_labels[i] + ' ' + thresholds[i] + ' 0 1')
-    os.system('python generate_alphas_ahab.py iris_2d 1 boss_iris_2d_' + savekey + '_' + threshold_labels[i] + ' ' + thresholds[i] + ' 0 1')
-os.system('python generate_alphas_ahab.py iris_2d 1 boss_iris_2d_north_' + savekey + ' 10 1 1')
-os.system('python generate_alphas_ahab.py iris_2d 1 boss_iris_2d_south_' + savekey + ' 10 2 1')
+    # os.system('python generate_alphas_ahab.py iris_1d 1 boss_iris_1d_' + savekey + '_' + threshold_labels[i] + ' ' + thresholds[i] + ' 0 1 0')
+    os.system('python generate_alphas_ahab.py iris_2d 1 boss_iris_2d_' + savekey + '_' + threshold_labels[i] + ' ' + thresholds[i] + ' 0 1 0')
+os.system('python generate_alphas_ahab.py iris_2d 1 boss_iris_2d_north_' + savekey + ' 10 1 1 0')
+os.system('python generate_alphas_ahab.py iris_2d 1 boss_iris_2d_south_' + savekey + ' 10 2 1 0')
+
 
 #now calculate bootstrap samples:
-os.system('python bootstrap_ahab.py sdss_1d_' + savekey + ' 10000')
-os.system('python bootstrap_ahab.py sdss_iris_1d_' + savekey + ' 10000')
-os.system('python bootstrap_ahab.py sdss_2d_' + savekey + ' 10000')
-os.system('python bootstrap_ahab.py sdss_iris_2d_' + savekey + ' 10000')
+# os.system('python bootstrap_ahab.py sdss_1d_' + savekey + ' 10000')
+# os.system('python bootstrap_ahab.py sdss_iris_1d_' + savekey + ' 10000')
+# os.system('python bootstrap_ahab.py sdss_2d_' + savekey + ' 10000')
+# os.system('python bootstrap_ahab.py sdss_iris_2d_' + savekey + ' 10000')
 for i in range(len(thresholds)):
-    os.system('python bootstrap_ahab.py boss_iris_1d_' + savekey + '_' + threshold_labels[i] + ' 10000')
+    # os.system('python bootstrap_ahab.py boss_iris_1d_' + savekey + '_' + threshold_labels[i] + ' 10000')
     os.system('python bootstrap_ahab.py boss_iris_2d_' + savekey + '_' + threshold_labels[i] + ' 10000')
 os.system('python bootstrap_ahab.py boss_iris_2d_north_' + savekey + ' 10000')
 os.system('python bootstrap_ahab.py boss_iris_2d_south_' + savekey + ' 10000')
+
+# after this, need to calculate bootstrap envelopes
+
