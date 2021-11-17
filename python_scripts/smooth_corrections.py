f66b5edacdf9e035c11577db0a81656f51233901
Blake Chellew <blakechellew@dhcp-v243-032.pv.reshsg.uci.edu>
2021-11-16 22:55:41 -0800

    Add option to specify load key

--- python_scripts/smooth_corrections.py
+++ python_scripts/smooth_corrections.py
@@ -8,7 +8,9 @@ from astropy.io import fits
 wav_boss = np.load('../alphas_and_stds/wavelength_boss.npy')
 hdulist = fits.open( '../data/SDSS_allskyspec.fits')
 wav_sdss = np.array(hdulist[1].data)
+loadkey = '111521'
 
+"""
 # Smooth the i100
 i100_files = [
     '../alphas_and_stds/avg_i100_boss_iris.npy',
@@ -28,6 +30,7 @@ for i, i100 in enumerate(i100s):
     np.save(i100_files[i].split('.npy')[0] + '_smooth.npy', i100_mean_smooth)
 
 exit(0)
+"""
 
 ######################
 # Now smooth the correction factors
@@ -35,13 +38,13 @@ exit(0)
 
 # BOSS
 files_boss = [
-    '../alphas_and_stds/correction_factor_boss_iris_north.npy',
-    '../alphas_and_stds/correction_factor_boss_iris_south.npy',
-    '../alphas_and_stds/correction_factor_boss_iris.npy',
-    '../alphas_and_stds/correction_factor_boss_iris_15.npy',
-    '../alphas_and_stds/correction_factor_boss_iris_20.npy',
-    '../alphas_and_stds/correction_factor_boss_iris_25.npy',
-    '../alphas_and_stds/correction_factor_boss_iris_30.npy'
+    '../alphas_and_stds/correction_factor_boss_iris_north_' + loadkey + '.npy',
+    '../alphas_and_stds/correction_factor_boss_iris_south_' + loadkey + '.npy',
+    '../alphas_and_stds/correction_factor_boss_iris_2d_' + loadkey + '_10.npy',
+    '../alphas_and_stds/correction_factor_boss_iris_2d_' + loadkey + '_15.npy',
+    '../alphas_and_stds/correction_factor_boss_iris_2d_' + loadkey + '_20.npy',
+    '../alphas_and_stds/correction_factor_boss_iris_2d_' + loadkey + '_25.npy',
+    '../alphas_and_stds/correction_factor_boss_iris_2d_' + loadkey + '_30.npy'
 ]
 correction_factors_boss = [np.load(file) for file in files_boss]
 
@@ -59,11 +62,12 @@ for i, corr in enumerate(correction_factors_boss):
     plt.show()
     np.save(files_boss[i].split('.npy')[0] + '_smooth.npy', corr_smooth)
 
+"""
 for i, corr in enumerate(correction_factors_sdss):
     corr_smooth = ndimage.gaussian_filter(corr, 30)
     plt.plot(wav_sdss, corr)
     plt.plot(wav_sdss, corr_smooth)
     plt.show()
     np.save(files_sdss[i].split('.npy')[0] + '_smooth.npy', corr_smooth)
-
+"""
 
